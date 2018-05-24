d2a20548a9f3c2ba6aaf64c343682385b6e0c96c
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-05-24 12:31:10 +0200

    add show route and template

--- app.js
+++ app.js
@@ -53,6 +53,18 @@ app.post('/blogs', function (req, res) {
     });
 });
 
+//Show route
+app.get('/blogs/:id', function (req, res) {
+    Blog.findById(req.params.id, function (err, foundBlog) {
+       if (err) {
+           res.redirect('/blogs');
+       } else {
+           res.render('show', {blog: foundBlog});
+       }
+    });
+});
+
+
 app.listen(3000, function () {
     console.log('Server is running');
 });
